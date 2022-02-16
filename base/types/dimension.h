ce69262febbb31dfacdc0015271107ff6d5e0eaa
lstrsrt <79076531+lstrsrt@users.noreply.github.com>
2022-02-16 23:47:24 +0100

    Update dimension.h

--- base/types/dimension.h
+++ base/types/dimension.h
@@ -3,9 +3,80 @@
 struct d2 {
     int x{ }, y{ };
 
+    constexpr d2& operator+=(const d2& rhs) noexcept
+    {
+        x += rhs.x;
+        y += rhs.y;
+        return *this;
+    }
+
+    constexpr d2& operator-=(const d2& rhs) noexcept
+    {
+        x -= rhs.x;
+        y -= rhs.y;
+        return *this;
+    }
+
+    constexpr d2& operator*=(const d2& rhs) noexcept
+    {
+        x *= rhs.x;
+        y *= rhs.y;
+        return *this;
+    }
+
+    constexpr d2& operator/=(const d2& rhs) noexcept
+    {
+        x /= rhs.x;
+        y /= rhs.y;
+        return *this;
+    }
+
+    constexpr d2& operator+=(const float rhs) noexcept
+    {
+        x += rhs;
+        y += rhs;
+        return *this;
+    }
+
+    constexpr d2& operator-=(const float rhs) noexcept
+    {
+        x -= rhs;
+        y -= rhs;
+        return *this;
+    }
+
+    constexpr d2& operator*=(const float rhs) noexcept
+    {
+        x *= rhs;
+        y *= rhs;
+        return *this;
+    }
+
+    constexpr d2& operator/=(const float rhs) noexcept
+    {
+        x /= rhs;
+        y /= rhs;
+        return *this;
+    }
+
     d2 operator+(const int rhs) const noexcept
     {
         return d2(x + rhs, y + rhs);
+    }
+	
+	d2 operator-(const int rhs) const noexcept
+    {
+        return d2(x - rhs, y - rhs);
+    }
+	
+    d2 operator*(const int rhs) const noexcept
+    {
+        return d2(x * rhs, y * rhs);
+    }
+	
+	d2 operator/(const int rhs) const noexcept
+    {
+        return d2(x / rhs, y / rhs);
     }
 
     d2 operator+(const d2& rhs) const noexcept
@@ -16,5 +87,15 @@ struct d2 {
     d2 operator-(const d2& rhs) const noexcept
     {
         return d2(x - rhs.x, y - rhs.y);
+    }
+	
+    d2 operator*(const d2& rhs) const noexcept
+    {
+        return d2(x * rhs.x, y * rhs.y);
+    }
+	
+	d2 operator/(const d2& rhs) const noexcept
+    {
+        return d2(x / rhs.x, y / rhs.y);
     }
 };
